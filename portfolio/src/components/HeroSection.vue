c5ab50fa33cf9d6b73919162243d46f1dc8ba8fc
FaridGoldmann <zult1337@protonmail.com>
2021-11-24 23:14:14 +0100

    created project and started landing page
    created components for navbar, button, hero section and landing page

---
+++ portfolio/src/components/HeroSection.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="hero">
+        <h1>
+            Goldmann,
+            <br>
+            <u>UX/UI-Designer</u>
+            
+        </h1>
+        <p>My name is Farid Goldmann and I’m a passionate designer based in Vienna, Austria.</p>
+        <a href=""><Button cap="View my work"/></a>
+        <ul class="social-icons">
+            <li><a href=""><img src="../assets/social-icons/linked_in.svg" alt="LinkedIn Logo"></a></li>
+            <li><a href=""><img src="../assets/social-icons/xing.svg" alt="Xing Logo"></a></li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+import Button from './Button.vue'
+export default ({
+
+    name: 'HeroSection',
+    components: {
+        Button
+    }
+})
+</script>
+
+<style scoped>
+    p   {
+        font-family: "Biotif-Book";
+        font-size: 1.6rem;
+        margin-bottom: 18px;
+    }
+
+    .hero   {
+        height: 100vh;
+        display: flex;
+        align-items: flex-start;
+        justify-content: center;
+        flex-direction: column;
+    }
+    .social-icons   {
+        position: absolute;
+        bottom: 0;
+        margin-bottom: 4vh;
+    }
+</style>
---
+++ portfolio/src/components/LandingPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <HeroSection/>
+    </main>
+</template>
+
+<script>
+import HeroSection from './HeroSection.vue'
+
+export default  ({
+    setup() {
+        
+    },
+    name: 'LandingPage',
+    components: {
+        HeroSection
+    }
+})
+</script>
+
+<style scoped>
+    main    {
+        margin: 0 80px;
+    }
+</style>
